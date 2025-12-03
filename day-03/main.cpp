a30701c5d84ae7ae19716b3fac6e80815bed7111
Karol Gryc <kg306015@student.polsl.pl>
2025-12-03 21:48:52 +0100

    Day 3 solved - 2*

---
+++ day-03/main.cpp
@@ -0,0 +1,103 @@
+#include <print>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <stdexcept>
+
+#include "../utils/utils.hpp"
+
+std::vector<int> string_to_int_vec(const std::string& str) {
+    std::vector<int> res(str.size());
+    for (int i = 0; i < str.size(); i++) {
+        res[i] = std::stoi(std::string(1, str[i]));
+    }
+
+    return res;
+}
+
+std::vector<std::vector<int>> parseLines(const std::vector<std::string>& lines) {
+    std::vector<std::vector<int>> parsedLines;
+    for (const auto& line : lines) {
+        auto parsed = string_to_int_vec(line);
+        parsedLines.push_back(parsed);
+    }
+
+    return parsedLines;
+}
+
+uint64_t evalForTwoBatteries(const std::vector<int>& bank) {
+    uint64_t max{};
+    for (int i = 0; i < bank.size(); i++) {
+        for (int j = i + 1; j < bank.size(); j++) {
+            uint64_t val = bank[i] * 10 + bank[j];
+            max = std::max(val, max);
+        }
+    }
+
+    return max;
+}
+
+uint64_t evallAllLinesForTwoBatteries(const std::vector<std::vector<int>>& banks) {
+    uint64_t sum{};
+    for (const auto& bank : banks) {
+        sum += evalForTwoBatteries(bank);
+    }
+
+    return sum;
+}
+
+uint64_t evalForNBatteries(const std::vector<int>& bank, int n) {
+    if (n <= 0 || n > bank.size()) {
+        throw std::string("Input error");
+    }
+
+    std::vector<int> solution(bank.end() - n, bank.end());
+    std::vector<int> rest(bank.begin(), bank.end() - n);
+    std::reverse(rest.begin(), rest.end());
+
+    for (auto el: rest) {
+        int tested = el;
+        for (int i = 0; i < solution.size(); i++) {
+            if (tested < solution[i]) {
+                break;
+            }
+            
+            std::swap(solution[i], tested);
+        }
+    }
+
+    uint64_t num{};
+    for (auto el : solution) {
+        num *= 10;
+        num += el;
+    }
+
+    return num;
+}
+
+uint64_t evalAllForNBatteries(const std::vector<std::vector<int>>& banks, int n) {
+    uint64_t sum{};
+    for (const auto& bank: banks) {
+        sum += evalForNBatteries(bank, n);
+    }
+
+    return sum;
+}
+
+int main() {
+    // test eval
+    auto lines = loadFile("test.txt");
+    auto parsedLines = parseLines(lines);
+    std::print("Evaluated test: {}\n", evallAllLinesForTwoBatteries(parsedLines));
+    std::print("Evalueated for 12 batteries: {}\n", evalAllForNBatteries(parsedLines, 12));
+
+    // actual eval
+    lines = loadFile("input.txt");
+    parsedLines = parseLines(lines);
+    std::print("Evaluated input.txt: {}\n", evallAllLinesForTwoBatteries(parsedLines));
+    std::print("Evauluated input.txt for 12 batteries: {}\n", evalAllForNBatteries(parsedLines, 12));
+
+    return 0;
+}
